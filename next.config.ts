0649839dc39be3f8b8eada964154f970d16d2dfd
mactar27 <mactar27@example.com>
2026-04-25 00:12:35 +0000

    Fix: Correction de la configuration Next.js pour le build

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["192.168.1.21"],
   devIndicators: {
-    appIsrStatus: false,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right",
   },
 };
 
